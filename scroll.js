fd090a5693321ebe94d168a0d7e9f34c506438b6
Daniel Gleason <dgleason@gmail.com>
2022-02-04 10:13:37 -0800

    Adding toggle banner based on scroll

---
+++ scroll.js
@@ -0,0 +1,16 @@
+bannerLogo = document.getElementById('bannerLogo');
+bannerLinks = document.getElementById('bannerLinks');
+
+var scroll_func = function()
+{
+    var y = window.scrollY;
+    if (y >= 50) {
+        bannerLogo.className = "logo show"
+        bannerLinks.className = "links show"
+    } else {
+        bannerLogo.className = "logo hide"
+        bannerLinks.className = "links hide"
+    }
+};
+
+window.addEventListener("scroll", scroll_func);
